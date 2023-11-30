88bb48dad2b6aac1a99b1ccebb8054d9c3bedc9f
fabio <fabio@gmail.com>
2023-11-30 13:28:02 -0300

    Lista de Tarefas

---
+++ tarefas/src/components/Search.jsx
@@ -0,0 +1,10 @@
+const Search = ({ search, setSearch }) => {
+    return ( 
+        <div className='search'>
+            <h1>Pesquisar:</h1>
+            <input type="text" value={search} onChange={(e) => setSearch(e.target.value)} placeholder='Digite para pesquisar'x />
+        </div>
+    );
+}
+
+export default Search;
